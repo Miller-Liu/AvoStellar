d4f4c4fe0d4ad45220ff983526cfa3acfd287965
Miller-Liu <millerliu@berkeley.edu>
2024-03-17 16:59:08 -0700

    Skeleton Code

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
+import Main from './pages/Main';
+import Shop from './pages/Shop';
+import Signup from './pages/Signup';
+import Login from './pages/Login';
 import './App.css';
+
+import { Route, Routes} from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Routes>
+        <Route path="/shop" element={<Shop/>}></Route>
+        <Route path="/signup" element={<Signup/>}></Route>
+        <Route path="/main" element={<Main/>}></Route>
+        <Route path="/login" element={<Login/> }></Route>
+      </Routes>
     </div>
   );
 }
