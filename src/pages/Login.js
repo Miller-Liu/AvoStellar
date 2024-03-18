54562f10c3288c441c3b8d777203ee0abba1d62d
Miller-Liu <millerliu@berkeley.edu>
2024-03-17 18:29:27 -0700

    temp

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,8 +1,19 @@
+import "../css/login_signup.css";
 
 export default function Login() {
     return (
-        <div>
-            Login
+        <div className="main-container">
+            <div className="container">
+                <div className="greeting">
+                    hi
+                </div>
+                <div>
+                    waving
+                </div>
+                <div>
+                    bye
+                </div>
+            </div>
         </div>
     )
 }
